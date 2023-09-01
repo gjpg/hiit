a2719cba4726308397284dab08a5be6192fc068e
Alastair Gibb <alastair.gibb@hbsuk.co.uk>
2023-09-01 08:58:42 +0100

    Added SVG experiment

--- src/routes/hiit/index.tsx
+++ src/routes/hiit/index.tsx
@@ -1,7 +1,7 @@
-import { component$, $ } from "@builder.io/qwik";
+import { component$, $ } from '@builder.io/qwik';
 
 interface CircleProps {
-    labelSegment: number; // 0 -> 1
+  labelSegment: number; // 0 -> 1
 }
 
 //positive offset = anti-clockwise
@@ -16,121 +16,119 @@ interface CircleProps {
 //so:
 //((-0.25 * pathLength) + ((labelSegment/2) * pathLength))
 
-export default component$(({labelSegment = 0.3}: CircleProps) => {
-    const radius = 90;
-    const pathLength = radius * 2 * Math.PI;
-    const offset = ((-0.25 * pathLength) + ((labelSegment / 2) * pathLength));
+interface Arc {
+  radius: number;
+  start: number; // 0 -> 1, 0 == 6 O'Clock
+  end: number;
+  width: number;
+  colour: string;
+}
 
-    //user defined run parameters
-    const warmup = 300;
-    const sprint = 40;
-    const rest = 60;
-    const sprintCount = 5;
-    //need to type warmup, sprint and rest as something other than numbers
+export default component$(({ labelSegment = 0.25 }: CircleProps) => {
+  const radius = 90;
+  const pathLength = radius * 2 * Math.PI;
+  const offset = -0.25 * pathLength + (labelSegment / 2) * pathLength;
 
-    console.log("pathLength", pathLength);
-    console.log("offset", offset);
+  //user defined run parameters
+  const warmup = 300;
+  const sprint = 40;
+  const rest = 60;
+  const sprintCount = 5;
+  //need to type warmup, sprint and rest as something other than numbers
 
-    //add it all up to 1
-    //eg. label segment = 0.4
-    //(2 * warmup) + (sprintCount * sprint) - ((sprintCount - 1) * rest) = (1 - labelSegment) * pathLength
+  console.log('pathLength', pathLength);
+  console.log('offset', offset);
 
+  //add it all up to 1
+  //eg. label segment = 0.4
+  //(2 * warmup) + (sprintCount * sprint) - ((sprintCount - 1) * rest) = (1 - labelSegment) * pathLength
 
-    //how much path is left after the labelSegment is accounted for
-    const pathRemainder = (1 - labelSegment) * pathLength
-    console.log("pathRemainder", pathRemainder)
+  //how much path is left after the labelSegment is accounted for
+  const pathRemainder = (1 - labelSegment) * pathLength;
+  console.log('pathRemainder', pathRemainder);
 
-    //tally up how many seconds the run will last
-    const sumSeconds = (2 * warmup) + (sprintCount * (sprint + rest)) - ((sprintCount - 1) * rest)
-    console.log("sumSeconds", sumSeconds)
+  //tally up how many seconds the run will last
+  const sumSeconds = 2 * warmup + sprintCount * (sprint + rest) - (sprintCount - 1) * rest;
+  console.log('sumSeconds', sumSeconds);
 
-    //divide remaining space by length of run
-    const squeezeDown = (pathRemainder / sumSeconds)
-    console.log("squeezeDown", squeezeDown)
+  //divide remaining space by length of run
+  const squeezeDown = pathRemainder / sumSeconds;
+  console.log('squeezeDown', squeezeDown);
 
-    return(
-        <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300">
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300">
+      <circle cx="50%" cy="50%" r={radius} stroke="none" stroke-width="20" fill="none" />
 
+      <circle
+        cx="50%"
+        cy="50%"
+        r={radius}
+        stroke="red"
+        stroke-width="20"
+        fill="none"
+        stroke-dashoffset={[offset + labelSegment * pathLength]}
+        stroke-dasharray={[warmup, sprint, rest, sprint, rest, sprint, rest, sprint, rest, sprint, warmup].map(
+          (x, index) => (index === 0 ? x : x * squeezeDown),
+        )}
+      />
 
-
-    <circle
-            cx="50%" cy="50%" r={radius}
-            stroke="none"
-            stroke-width="20"
-            fill="none" />
-
-
-    <circle
-            cx="50%" cy="50%" r={radius}
-            stroke="red"
-            stroke-width="20"
-            fill="none"
-            stroke-dasharray={[(labelSegment * pathLength), warmup, sprint, rest, sprint, rest, sprint, rest, sprint, rest, sprint, warmup].map((x, index) => index===0? x : x * squeezeDown)}
-            stroke-dashoffset={[offset]}
-            />
-
-
-    <circle
-            cx="50%" cy="50%" r={radius}
-            stroke="blue"
-            stroke-width="5"
-            fill="none"
-            stroke-dashoffset={[offset]}
-            stroke-dasharray={[labelSegment, 1 - labelSegment].map(l => l * pathLength)}
-    />
-
-</svg>
-
-    )
-
+      <circle
+        cx="50%"
+        cy="50%"
+        r={radius}
+        stroke="blue"
+        stroke-width="5"
+        fill="none"
+        transform={`rotate(180, 150, 150)`}
+        stroke-dasharray={[labelSegment, 1 - labelSegment].map((l) => l * pathLength)}
+      />
+    </svg>
+  );
 });
 
 interface CircleParameters {
+  width: number;
+  height: number;
+  radius: number;
+  strokeWidth: number;
 
-    width: number;
-    height: number;
-    radius: number;
-    strokeWidth: number;
+  colourRest: string;
+  //green
 
-    colourRest: string;
-    //green
+  colourRun: string;
+  //red
+  arrayRun: number;
+  //inputs needed: warmup length, rest length, run length, run count
+  //from that, calculate total length
+  //lengthTotal = ((2 * warmup) + (count * (rest + run)) - rest)
+  //to account for the space taken up by the bottom segment:
+  //(1 - portionSegment) * 2 * π * radius
+  //to make it look clean:
+  //share = 2 * π * radius / lengthTotal
+  //should look like this:
+  //stroke-dasharray="(1 - portionSegment) * share)) (warmup * share) (run * share) (rest * share) (run * share) (warmup * share)"
 
-    colourRun: string;
-    //red
-    arrayRun: number;
-    //inputs needed: warmup length, rest length, run length, run count
-    //from that, calculate total length
-    //lengthTotal = ((2 * warmup) + (count * (rest + run)) - rest)
-    //to account for the space taken up by the bottom segment:
-    //(1 - portionSegment) * 2 * π * radius
-    //to make it look clean:
-    //share = 2 * π * radius / lengthTotal
-    //should look like this:
-    //stroke-dasharray="(1 - portionSegment) * share)) (warmup * share) (run * share) (rest * share) (run * share) (warmup * share)"
+  colourSegment: string;
+  //blue
+  portionSegment: number;
+  //to determine the portion that the bottom segment fills
+  //eg. 0.4
+  //cannot be greater or equal to 1
+  //cannot be less than or equal to 0
+  arraySegment: dunno;
+  //composed of 2 parts
+  //portionSegment * 2 * π * radius
+  //(1 - portionSegment) * 2 * π * radius
 
-    colourSegment: string;
-    //blue
-    portionSegment: number;
-    //to determine the portion that the bottom segment fills
-    //eg. 0.4
-    //cannot be greater or equal to 1
-    //cannot be less than or equal to 0
-    arraySegment: dunno;
-    //composed of 2 parts
-    //portionSegment * 2 * π * radius
-    //(1 - portionSegment) * 2 * π * radius
-    
-    offset: number;
-    //to align bottom segment (for both Run and Segment)
-    //portionSegment * π * radius
-
-};
+  offset: number;
+  //to align bottom segment (for both Run and Segment)
+  //portionSegment * π * radius
+}
 
 //<svg xmlns="http://www.w3.org/2000/svg" width="width" height="height">
 
-
 //<circle class="circleRest">
-//cx="50%" cy="50%" 
+//cx="50%" cy="50%"
 //fill="none"
 //
 //r="radius"
@@ -139,9 +137,8 @@ interface CircleParameters {
 //stroke=colourRest
 ///>
 
-
 //<circle class="circleRun">
-//cx="50%" cy="50%" 
+//cx="50%" cy="50%"
 //fill="none"
 //
 //r="radius"
@@ -154,9 +151,8 @@ interface CircleParameters {
 ////stroke-dashoffset="offset"
 ///>
 
-
 //<circle class="circleSegment">
-//cx="50%" cy="50%" 
+//cx="50%" cy="50%"
 //fill="none"
 //
 //r="radius"
@@ -168,6 +164,5 @@ interface CircleParameters {
 //
 //stroke-dashoffset="offset"
 ///>
-
 
 //</svg>
