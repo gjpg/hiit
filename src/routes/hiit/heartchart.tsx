9a0cd4eb9df16db35119e337e645ddfbbe8f01ad
George <george.j.p.gibb@googlemail.com>
2023-10-01 12:16:02 +0100

    new chart

--- src/routes/hiit/heartchart.tsx
+++ src/routes/hiit/heartchart.tsx
@@ -2,33 +2,18 @@ import { component$, useContext, useSignal, useVisibleTask$ } from '@builder.io/
 import { Chart, ChartDataset, Filler, registerables } from 'chart.js';
 import { InputContext, useHIITContext } from '~/routes/hiit/contexts';
 
-export const BarChart = component$(() => {
-  const myChart1 = useSignal<HTMLCanvasElement>();
-  const myChart2 = useSignal<HTMLCanvasElement>();
-  // const dataset1 = [10, 15, 7, 12];
-  // const dataset2 = [8, 11, 9, 14];
-  // const barWidth = 10;
-
-  // Define your data for each dataset
-  const dataset = [10, 15, 7, 12, 8, 11, 9, 14, 6, 18, 5, 13, 11, 10];
-  const allLabels = dataset.map((d) => `${d}`);
-
-  // // Determine the total number of data points
-  // const totalDataPoints = datasets.reduce((total, dataset) => total + dataset.length, 0);
-  //
-  // // Create an array to hold all labels for all datasets
-  // const allLabels = Array.from({ length: totalDataPoints }, (_, i) => `Label ${i + 1}`);
-  //
-  // // Create an array to hold the combined data for all datasets
-  // const allData = datasets.reduce((combinedData, dataset) => combinedData.concat(dataset), []);
-
-  // const ctx = document.getElementById('myChart').getContext('2d');
+interface PhaseHeartRateProps {
+  dataset: number[];
+  phaseColour: string;
+}
+export const PhaseHeartRates = component$<PhaseHeartRateProps>(({ dataset, phaseColour }) => {
+  const chart = useSignal<HTMLCanvasElement>();
 
   useVisibleTask$(() => {
-    if (myChart1?.value) {
-      const phaseColour = 'green';
+    if (chart?.value) {
+      const allLabels = dataset.map((d) => `${d}`);
       Chart.register(Filler, ...registerables);
-      new Chart(myChart1.value, {
+      new Chart(chart.value, {
         type: 'line',
         data: {
           labels: allLabels,
@@ -43,37 +28,8 @@ export const BarChart = component$(() => {
           ],
         },
         options: {
-          elements: { point: { radius: 0 } },
-          plugins: {
-            legend: { display: false },
-            tooltip: { enabled: false },
-          },
-          scales: {
-            x: {
-              display: false, // Hide the x-axis labels
-            },
-            y: { beginAtZero: true, display: false },
-          },
-        },
-      });
-    }
-    if (myChart2?.value) {
-      new Chart(myChart2.value, {
-        type: 'line',
-        data: {
-          labels: allLabels,
-          datasets: [
-            {
-              label: 'Combined Dataset',
-              data: dataset,
-              borderColor: 'rgba(75, 192, 192, 1)',
-              borderWidth: 2,
-              fill: false,
-            },
-          ],
-        },
-        options: {
-          backgroundColor: 'red',
+          responsive: true,
+          maintainAspectRatio: false,
           elements: { point: { radius: 0 } },
           plugins: {
             legend: { display: false },
@@ -91,16 +47,28 @@ export const BarChart = component$(() => {
   });
 
   return (
-    <div class="charts">
-      <div style={{ backgroundColor: 'green' }}>
-        <canvas ref={myChart1}></canvas>
-      </div>
-      <div style={{ backgroundColor: 'red' }}>
-        <canvas ref={myChart2}></canvas>
-      </div>
+    <div style={{ backgroundColor: phaseColour }}>
+      <canvas style={{ height: '100%' }} ref={chart}></canvas>
     </div>
   );
 });
+
+export const BarChart = component$(() => {
+  const dataset = [10, 15, 7, 12, 8, 11, 9, 14, 0, 10];
+  const dataset1 = [...dataset, ...dataset, ...dataset, ...dataset];
+  const dataset2 = [...dataset, ...dataset];
+  const dataset3 = [1, 5, 6, 3, 2];
+  const allPhases = [dataset1, dataset3, dataset2];
+
+  return (
+    <div class="charts" style={{ gridTemplateColumns: `${allPhases.map((ds) => ds.length + 'fr').join(' ')}` }}>
+      {allPhases.map((p, i) => (
+        <PhaseHeartRates dataset={p} phaseColour={i % 2 ? 'green' : 'red'} />
+      ))}
+    </div>
+  );
+});
+
 export const HeartChart = component$(() => {
   const myChart = useSignal<HTMLCanvasElement>();
   const { workoutDuration, now, centreHeight, centreWidth, radius } = useHIITContext();
@@ -161,6 +129,8 @@ export const HeartChart = component$(() => {
         type: 'line',
         data: data,
         options: {
+          responsive: true,
+          maintainAspectRatio: false,
           elements: { point: { radius: 0 } },
           plugins: {
             legend: { display: false },
